5a96e45b7e1f9512cb78d2d9ccf06d97863ead73
Vanessa <blackkoreangirl@gmail.com>
2025-04-08 18:37:05 -0400

    routes set up for each page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h2 className="text-black text-xl">About Page</h2>
+      <p>This is the place where you can learn more about Astute Abroad!</p>
+    </div>
+  )
+}

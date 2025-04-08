aae5c70d76be606ad070d00e002dbae9f1d34a6e
Vanessa <blackkoreangirl@gmail.com>
2025-04-08 14:09:31 -0400

    header with logo and website greeting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,15 +16,21 @@ function App() {
 
   return (
     <div class="bg-sky-200 min-h-screen flex items-center justify-center">
-      <div class="w-[90%] border borderr-black rounded-xl flex flex-col shadow-lg">
-        <h1>Welcome to Astute Abroad</h1>
-        <a href="https://github.com/ChasVanDav/AstuteAbroad" target="_blank">
-          <img
-            src={astuteAbroadLogo}
-            className="logo"
-            alt="Astute Abroad logo"
-          />
-        </a>
+      <div class="w-[90%] border border-black rounded-xl flex flex-col shadow-lg">
+        {/* header */}
+        <header class="flex items-center p-4 border-b border-black">
+          <a href="https://github.com/ChasVanDav/AstuteAbroad" target="_blank">
+            <img
+              src={astuteAbroadLogo}
+              className="w-16 h-16 rounded-full mr-4"
+              alt="Astute Abroad logo"
+            />
+          </a>
+          <h1 classname="text-3xl font-bold text-black">
+            Welcome to Astute Abroad
+          </h1>
+        </header>
+
         <div className="bg-sky-400 text-black p-4 rounded">
           <button onClick={() => setCount(count + 1)}>count is {count}</button>
         </div>

07dc9b6ea8cadd8005782f4f47987b4dde289d3f
Vanessa <blackkoreangirl@gmail.com>
2025-04-08 16:50:42 -0400

    navbar and header layout and color scheme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,45 +16,42 @@ function App() {
 
   return (
     <div className="bg-sky-200 min-h-screen flex items-center justify-center">
-      <div className="w-[90%] border border-black rounded-xl flex flex-col shadow-lg">
+      <div className="bg-sky-300 w-[90%] border border-black rounded-xl flex flex-col shadow-lg">
         {/* header */}
-        <header className="flex items-center p-4 border-b border-black">
+        <header className="flex items-center justify-between p-4 border-b border-black">
+          <h1 className="bg-white text-3xl font-light text-black py-3 p-4 rounded-2xl border border-black">
+            Welcome to Astute Abroad!
+          </h1>
           <a href="https://github.com/ChasVanDav/AstuteAbroad" target="_blank">
             <img
               src={astuteAbroadLogo}
-              className="w-16 h-16 rounded-full mr-4"
-              alt="Astute Abroad logo"
+              className="w-20 h-24 mr-4"
+              alt="Astute Abroad logo - wise fox with top hat and monacle"
             />
           </a>
-          <h1 className="text-3xl font-bold text-black">
-            Welcome to Astute Abroad
-          </h1>
         </header>
 
         {/* body */}
         <div className="flex flex-1">
           {/* main content */}
-          <main className="flex-1 p-6 border-l border-black">
-            <h2 className="text-2xl font-bold text-black mb-4">Welcome!</h2>
+          <main className="flex-1 p-6">
+            <h2 className="text-2xl font-light text-black mb-4">Welcome!</h2>
             <p className="text-black mb-4">main content area</p>
           </main>
 
           {/* navigation bar - right side */}
-          <aside className="w-1/5 border-l border-black p-4 flex flex-col gap-4 items-stretch bg-sky-300">
-            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-gray-100 trransition">
-              placeholder
+          <aside className="w-1/3 border-l border-black p-4 flex flex-col gap-4 items-stretch bg-sky-300">
+            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-orange-200	 transition">
+              About
             </button>
-            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-gray-100 trransition">
-              placeholder
+            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-orange-200 transition">
+              Watch Demo
             </button>
-            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-gray-100 trransition">
-              placeholder
+            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-orange-200 transition">
+              Register
             </button>
-            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-gray-100 trransition">
-              placeholder
-            </button>
-            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-gray-100 trransition">
-              placeholder
+            <button className="bg-white text-black font-semihold py-3 rounded-2xl border border-black hover:bg-orange-200 transition">
+              Log in
             </button>
           </aside>
 

b787b9629b77cd792717d1bf5939f10d9a927aee
Vanessa <blackkoreangirl@gmail.com>
2025-05-12 14:57:28 -0400

    remove because its causing more issues

--- server/server.js
+++ server/server.js
@@ -24,13 +24,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use(
-  cors({
-    origin: "https://astute-abroad.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-)
+// app.use(
+//   cors({
+//     origin: "https://astute-abroad.vercel.app",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// )
 
 app.get("/", (req, res) => {
   res.send("Hello from the Astute Abroad's backend!")
